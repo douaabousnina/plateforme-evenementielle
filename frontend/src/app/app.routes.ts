cf48256bfa812baf9859fe45cb8e2059d886a035
Mariem Abida <mariem.abida@insat.ucar.tn>
2026-01-28 22:55:02 +0100

    added routes in global routes file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { reservationRoutes } from './features/reservation/reservation.routes';
 import { organizerRoutes } from './organizer/organizer.routes';
 
 export const routes: Routes = [
-    ...reservationRoutes
-
-...organizerRoutes ]
+    ...reservationRoutes, 
+    ...organizerRoutes
+]
