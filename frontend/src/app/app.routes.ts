10d9678fc837f017fa46056fcf4455cc04bf878e
Mariem Abida <mariem.abida@insat.ucar.tn>
2026-01-28 22:40:56 +0100

    updated routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,33 +1,5 @@
 import { Routes } from '@angular/router';
+import { organizerRoutes } from './organizer/organizer.routes';
 
 export const routes: Routes = [
-   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./organizer/pages/dashboard/dashboard').then(m => m.Dashboard)
-  },
- {
-    path: 'events',
-    loadComponent: () => import('./organizer/pages/events/events').then(m => m.Events)
-  },
-//   {
-//     path: 'events/create',
-//     loadComponent: () => import('./organizer/pages/event-create/event-create').then(m => m.EventCreate)
-//   },
-//   {
-//     path: 'marketing',
-//     loadComponent: () => import('./organizer/pages/marketing/marketing').then(m => m.Marketing)
-//   },
-//   {
-//     path: 'reports',
-//     loadComponent: () => import('./organizer/pages/reports/reports').then(m => m.Reports)
-//   },
-//   {
-//     path: 'settings',
-//     loadComponent: () => import('./organizer/pages/settings/settings').then(m => m.Settings)
-//   }
-];
+...organizerRoutes ]
